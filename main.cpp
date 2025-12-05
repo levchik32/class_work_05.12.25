8d7c0add5f346a5eb632a8c4b7d5af4292e8d5a6
levchik32 <lev.saldaev@gmail.com>
2025-12-05 12:39:13 +0300

    p_t and f_t

--- main.cpp
+++ main.cpp
@@ -1,4 +1,36 @@
+#include <iostream>
+
+namespace topit
+{
+  struct p_t
+  {
+    int x, y;
+  };
+  bool operator==(p_t, p_t);
+  bool operator!=(p_t, p_t);
+  struct f_t {p_t aa, bb;};
+
+  struct Idraw
+  {
+    virtual p_t begin() const = 0;
+    virtual p_t next(p_t) const = 0;
+    virtual ~IDraw();
+  }
+}
+
 int main()
 {
+  using topit::p_t;
+  p_t a{1, 0}, b{1, 0};
+  std::cout << (a == b) << '\n';
+}
 
+bool topit::operator==(p_t a, p_t b)
+{
+  return a.x == b.x && a.y == b.y;
+}
+
+bool topit::operator!=(p_t a, p_t b)
+{
+  return 1(a == b);
 }
